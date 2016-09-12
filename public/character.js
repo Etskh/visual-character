fbc785bb404f4672d48582a25dce8bf25d2c31bd
James Loucks <etskh@hotmail.com>
2016-09-11 20:24:17 -0700

    Moved scripts to right folder

---
+++ public/character.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+  var pages = [
+    'overworld',
+    'combat',
+    'magic',
+    'equipment',
+    'statistics',
+    'debug'
+  ];
+
+  function pageHandler(page) {
+    return function(){
+      var url = '/character/1/' + page;
+      $('#nav div').removeClass('active');
+      $.get(url, {}, function(reply) {
+        $('#content').html(reply);
+        $('#header').text(page);
+        $('#nav #' + page).addClass('active');
+      });
+    };
+  };
+
+  for( var i=0; i<pages.length; ++i) {
+    $('#nav #' + pages[i]).click( pageHandler(pages[i]));
+  }
+});
