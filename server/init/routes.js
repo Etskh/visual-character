3169bb4e2aaeb2c8531abeaadca5113abeaba2d8
James Loucks <etskh@hotmail.com>
2016-09-17 21:31:47 -0700

    Did a bunch of stuff

--- server/init/routes.js
+++ server/init/routes.js
@@ -81,73 +81,7 @@ router.get('/equipment', function(req, res) {
 
 
 
-router.post('/action', require('body-parser').json(), function (req, res) {
-  const user = res.locals.user
-
-  user.getActiveCharacter(
-    // empty
-  ).then(function(character) {
-    // Allow execution of arbitrary code (lol)
-
-    // TODO: Pull this out
-    var args = req.body['args[]']
-    var func = character[req.body.action]
-
-    if( !func ) {
-      return res.json({
-        'success': false,
-        'error': + req.body.action + ' is not a function',
-      })
-    }
-
-    if( typeof args !== 'object' ) {
-      args = [ args ]
-    }
-
-    return func.apply(this, args)
-  }).then(function(character) {
-
-    const json = {
-      'success': true,
-    }
-
-    switch(req.body.return) {
-    case 'equipment':
-      json.stats = {
-        'current_load': character.current_load,
-        'current_load_percentage': character.current_load_percentage,
-        //'equipment': character.equipment,
-        'carry_weight': character.carry_weight,
-      }
-    }
-
-    // TODO: Make this actually flexible
-    if( req.body.partial === 'newest-item' ) {
-
-      const newestItem = _.maxBy(character.equipment, 'id')
-
-      return res.render('equipment/item.partial.html', {
-        item: newestItem,
-        flash: req.body.flash,
-      }, function( err, html ) {
-        if(err) {
-          json.success = false
-          return res.json(json)
-        }
-
-        json.partial = html
-        return res.json(json)
-      })
-    }
-
-    return res.json(json)
-  }, function(error){
-    return res.json({
-      'success': false,
-      'error': error,
-    })
-  })
-})
+router.post('/action', require('body-parser').json(), require('../routes/action'))
 
 
 module.exports = router
---
+++ server/routes/action.js
@@ -0,0 +1,78 @@
+'use strict'
+
+
+const equipment = require('../lib/equipment')
+const _ = require('lodash')
+
+
+
+const handler = function (req, res) {
+  const user = res.locals.user
+
+  user.getActiveCharacter(
+    // empty
+  ).then(function(character) {
+    // Allow execution of arbitrary code (lol)
+
+    // TODO: Pull this out
+    var args = req.body['args[]']
+    var func = character[req.body.action]
+
+    if( !func ) {
+      return res.json({
+        'success': false,
+        'error': + req.body.action + ' is not a function',
+      })
+    }
+
+    if( typeof args !== 'object' ) {
+      args = [ args ]
+    }
+
+    return func.apply(this, args)
+  }).then(function(character) {
+
+    const json = {
+      'success': true,
+    }
+
+    switch(req.body.return) {
+    case 'equipment':
+      json.stats = {
+        'current_load': character.current_load,
+        'current_load_percentage': character.current_load_percentage,
+        //'equipment': character.equipment,
+        'carry_weight': character.carry_weight,
+      }
+    }
+
+    // TODO: Make this actually flexible
+    if( req.body.partial === 'newest-item' ) {
+
+      const newestItem = _.maxBy(character.equipment, 'id')
+
+      return res.render('equipment/item.partial.html', {
+        item: newestItem,
+        flash: req.body.flash,
+      }, function( err, html ) {
+        if(err) {
+          json.success = false
+          return res.json(json)
+        }
+
+        json.partial = html
+        return res.json(json)
+      })
+    }
+
+    return res.json(json)
+  }, function(error){
+    return res.json({
+      'success': false,
+      'error': error,
+    })
+  })
+}
+
+
+module.exports = handler
