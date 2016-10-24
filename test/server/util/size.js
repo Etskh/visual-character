b4c87b57ebd296cb1adb768bf52164ddd1204aad
Etskh <etskh@hotmail.com>
2016-10-23 18:24:42 -0700

    I did it again. But at least its just a test

---
+++ test/server/util/size.js
@@ -0,0 +1,25 @@
+
+const assert = require('assert')
+const expect = require('expect')
+
+
+describe('Util / Size', function() {
+
+  const size = require('../../../server/util/size')
+
+  it('Gets a size from the standard list', function(done) {
+    size.getByName('small')
+    .then(function(s) {
+      done()
+    })
+  })
+
+  it('Rejects if it\'s not the name of a size', function(done) {
+    size.getByName('This is not the name of a size')
+    .then(function(s) {
+      // empty
+    }, function(e) {
+      done()
+    })
+  })
+})
