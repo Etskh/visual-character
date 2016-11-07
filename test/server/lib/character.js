cc0405960b382bb87589b54e0dbce2ec8c6816c9
Etskh <etskh@hotmail.com>
2016-11-07 01:32:38 -0800

    Improves coverage

--- test/server/lib/character.js
+++ test/server/lib/character.js
@@ -1,15 +1,60 @@
 'use strict'
 
-var assert = require('assert')
-var expect = require('expect')
-
+const assert = require('assert')
+const expect = require('expect')
+const sinon = require('sinon')
+const fs = require('fs')
 
 describe('Character', function() {
 
-  var character = require('../../../server/lib/character')
+  const character = require('../../../server/lib/character')
 
   it('gets a character from data', function(done) {
     character.getById(1).then( function(character) {
+      done()
+    })
+  })
+
+  it('returns the error if it can\'t read the file', function(done) {
+
+    const errorMessage = 'This is an error';
+    sinon.stub(fs, "readFile", function( dataPath, callback ) {
+      return callback(errorMessage)
+    })
+
+    character.getById(1).then( function(character) {
+      assert(false)
+      done()
+    }, function(error) {
+      expect(error).toEqual(errorMessage)
+      fs.readFile.restore()
+      done()
+    })
+  })
+
+  it('returns the error if it can\'t read the file', function(done) {
+
+    const errorMessage = 'This is an error';
+    sinon.stub(JSON, "parse", function( contents ) {
+      throw new Error(errorMessage)
+    })
+
+    character.getById(1).then( function(character) {
+      assert(false)
+      done()
+    }, function(error) {
+      JSON.parse.restore()
+      done()
+    })
+  })
+
+
+  it('returns the error if it can\'t read the file', function(done) {
+    character.getById(999999999).then( function(character) {
+      assert(false)
+      done()
+    }, function(error) {
+      expect(error).toExist()
       done()
     })
   })
